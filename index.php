4448c8dcfdfdda7fb146adc71ccac4b2086a62d0
Chigozie Ekwonu <chygoz2@gmail.com>
2018-04-16 20:37:23 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	date_default_timezone_set("Africa/Lagos");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Stage One Challenge</title>				
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<img src="images/workstation2.jpg" alt="workstation" />
+	<p class="date"><?php echo date("l dS F, Y") ?></p>
+	<p class="time"><?php echo date("h:i a") ?></p>
+</body>
+</html>
